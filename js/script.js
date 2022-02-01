41aec54cc79fe213746cf9912d743827922978cd
Vasily Agafonov <vasilyagafonov@MacBook-Air-Vasily.local>
2022-02-02 01:55:43 +0300

    второй урок по js условия

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
 "use strict";
 
-// const numberOfFilms = prompt('сколько фильмов вы уже просмотрели?', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов', ''),
-//       b = prompt('На сколько оцените его', ''),
-//       c = prompt('Один из последних просмотренных фильмов', ''),
-//       d = prompt('На сколько оцените его', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
+if (4 == 9) {
+    console.log('ok');
+} else {
+    console.log('error');
+}
 
 
+const num = 50;
+
+if (num < 49) {
+    console.log('erros');
+} else if (num > 100) {
+    console.log('много');
+} else {
+    console.log('ok');
+}
+
+(num === 50) ? console.log('ok') : console.log('erros');
 
 
-// const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
+const num = 50;
 
 
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('нвреное');
+        break;
+    case 50:
+        console.log('в точку');
+        break;
+    default:
+        console.log('несегодня');
+        break;
+    
+}
